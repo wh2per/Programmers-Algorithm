ce437b96858d8a47c192caacda0ef769b2005a54
wh2per <93negaro@gmail.com>
2019-01-15 13:56:05 +0900

    H-Index

---
+++ Programmers/H-Index.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end(), greater<int>());
+
+    for (int i = 0; i < citations.size(); ++i) {
+        if (citations[i] < i + 1) {
+            return i;
+        }
+    }
+
+    return citations.size();
+}
