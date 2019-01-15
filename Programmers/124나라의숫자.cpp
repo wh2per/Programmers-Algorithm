ec27de15fb090104967d9aa5d62c38ba87c72f7f
wh2per <93negaro@gmail.com>
2019-01-15 13:55:09 +0900

    124나라의숫자

---
+++ Programmers/124나라의숫자.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+using namespace std;
+
+string solution(int n) {
+	string answer = "";
+	int arr[3] = { 1,2,4 };
+	int r = -1;
+	// i-1 ÀÚ¸®¼ö 
+	while (n > 0) {
+		n -= 1;
+		r = n % 3;
+		answer += to_string(arr[n % 3]);
+		n = n / 3;
+	}
+
+	reverse(answer.begin(), answer.end());
+
+	return answer;
+}
