6a0dc7f1d092b83f2bf8c6bd3c7048135e8b49f1
wh2per <93negaro@gmail.com>
2019-01-15 13:57:19 +0900

    위장

---
+++ Programmers/위장.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <map>
+
+using namespace std;
+
+int solution(vector<vector<string>> clothes) {
+	int answer = 0;
+	map<string, int> m;
+
+	for (int i = 0; i < clothes.size(); i++) {
+		if (m.find(clothes[i][1]) != m.end()) 	//있을 떄
+			m[clothes[i][1]]++;
+		else									//없을 떄
+			m.insert(make_pair(clothes[i][1], 1));
+	}
+	answer = 1;
+	for (auto it = m.begin(); it != m.end(); it++)
+		answer *= it->second + 1;
+
+	return answer - 1;
+}
