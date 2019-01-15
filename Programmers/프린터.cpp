5cc83a15163df5291481b3ad6a53ae586894e236
wh2per <93negaro@gmail.com>
2019-01-15 13:58:11 +0900

    프린터

---
+++ Programmers/프린터.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> priorities, int location) {
+	int answer = 0;
+	bool check[100] = { false, };			// 검사한 작업인지 체크
+	vector<int> temp = priorities;		// 우선순위 순으로 정렬
+	int p = priorities[location];		// 정답
+	int count = priorities.size();
+	int i = 0, j = 0;
+	int cp = 0;		// 현재 우선순위
+
+	sort(temp.begin(), temp.end(), greater<int>());
+	cp = temp[i];
+
+	while (count != 0) {
+		if ((check[j] == false) && (cp == priorities[j])) {
+			check[j] = true;
+			count--;
+			i++;
+			answer++;
+			if ((cp == p) && (j == location))
+				break;
+			cp = temp[i];
+		}
+		j++;
+		if (j == priorities.size())
+			j = 0;
+
+	}
+
+	return answer;
+}
