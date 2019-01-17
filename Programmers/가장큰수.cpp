9def94c08adfaf65e37d264e84fa972e539c353a
wh2per <93negaro@gmail.com>
2019-01-17 21:38:50 +0900

    구명보트

---
+++ Programmers/가장큰수.cpp
@@ -0,0 +1,39 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+bool Comp(const string& a, const string& b)
+{
+	if (a == b) return false;
+
+	string ab = a + b;
+	string ba = b + a;
+
+	if (ab > ba) return true;		
+	else return false;
+}
+
+
+string solution(vector<int> numbers) {
+	string answer = "";
+	int zero = 0;
+	vector<string> temp;
+
+	for (auto i : numbers)
+	{
+		if (i == 0) 
+			++zero;
+		temp.push_back(to_string(i));
+	}
+
+	// 000 °æ¿ì
+	if (zero == numbers.size()) return "0";
+
+	sort(temp.begin(), temp.end(), Comp);
+
+	for (auto s : temp) answer.append(s);
+
+	return answer;
+}
---
+++ Programmers/구명보트.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solution1(vector<int> people, int limit) {
+	int answer = 0;
+	int index = 0;
+	sort(people.begin(), people.end());
+
+	while (people.size()-index !=0) {
+		int end = people.size() - 1;
+
+		if (end == index) {		//한 명 남았을 때
+			answer++;
+			break;
+		}
+		else {
+			int sum = people[index] + people[end];
+			bool check = true;
+			while (sum > limit) {
+				people.pop_back();
+				answer++;
+				end = people.size() - 1;
+				if (end == index) {
+					check = false;
+					break;
+				}
+				sum = people[index] + people[end];
+			}
+			answer++;
+			if (check)
+				people.pop_back();
+			index++;
+		}
+		
+	}
+
+	return answer;
+}
+
+/*
+int solution(vector<int> people, int limit) {
+	int answer = 0;
+	int size = people.size();
+
+	sort(people.begin(), people.end());
+	reverse(people.begin(), people.end());
+
+	for (int i = 0, j = size - 1; i <= j; i++) {
+		if (people[i] + people[j] <= limit) {
+			j--;
+		}
+		answer++;
+	}
+	return answer;
+}
+*/
