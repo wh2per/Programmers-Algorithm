42680fe489e4d844b986a0a365ddd91ebfbe8a98
wh2per <93negaro@gmail.com>
2019-01-18 15:13:54 +0900

    직사각형별찍기

---
+++ Programmers/직사각형별찍기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+void solution(void) {
+	int a;
+	int b;
+	cin >> a >> b;
+	for (int i = 0; i < b; i++) {
+		for (int j = 0; j < a; j++)
+			cout << "*";
+		cout << '\n';
+	}
+}
