52af26de7891d7f7a8e2b2f74f45f244d665f7fa
wh2per <93negaro@gmail.com>
2019-01-18 15:30:05 +0900

    x만큼간격이있는n개의숫자

---
+++ Programmers/Lv1_x만큼간격이있는n개의숫자.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<long long> solution(int x, int n) {
+	vector<long long> answer;
+	long long temp = 0;
+	for (int i = 0; i < n; i++) {
+		temp += x;
+		answer.push_back(temp);
+	}
+	return answer;
+}
