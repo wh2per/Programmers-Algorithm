16f7f304a56e7f2386536814bfebbc473e1ec576
wh2per <93negaro@gmail.com>
2019-01-24 15:03:29 +0900

    Lv3_타일장식물

---
+++ Programmers/Lv3/Lv3_타일장식물.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+long long solution82(int N) {
+	long long d[81];
+	long long s[81];
+	d[0] = 0;
+	d[1] = 1;
+	s[1] = 4 * d[1];
+	for (int i = 2; i <= N; i++) {
+		d[i] = d[i - 1] + d[i - 2];
+		s[i] = s[i - 1] - d[i] + d[i] * 3;
+	}
+	long long answer = s[N];
+	return answer;
+}
