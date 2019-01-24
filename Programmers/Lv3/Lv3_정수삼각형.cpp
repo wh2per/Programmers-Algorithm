3cb0a2efc1aa9f769fd5a095f9dab56acd7617f5
wh2per <93negaro@gmail.com>
2019-01-24 17:34:08 +0900

    Lv3_정수삼각형

---
+++ Programmers/Lv3/Lv3_정수삼각형.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+using namespace std;
+
+int solution83(vector<vector<int>> triangle) {
+	int answer = triangle[0][0];
+
+	if(triangle.size()==1)
+		return answer;
+
+	for (int i = 1; i < triangle.size(); i++) {			// 2 รþบฮลอ
+		for (int j = 0; j <=i; j++) {
+			if (j == 0) 
+				triangle[i][j] += triangle[i - 1][j];
+			else if (j == triangle[i].size()-1) 
+				triangle[i][j] += triangle[i - 1][j-1];
+			else 
+				triangle[i][j] += max(triangle[i - 1][j], triangle[i - 1][j-1]);
+		}
+	}
+
+ 	for (int i = 0; i < triangle.size(); i++) {
+		if (answer < triangle[triangle.size() - 1][triangle.size() - 1 - i])
+			answer = triangle[triangle.size() - 1][triangle.size() - 1 - i];
+	}
+	return answer;
+} 
+
+int main() {
+	vector<vector<int>> triangle = { {7},{3,8},{8, 1, 0},{2, 7, 4, 4},{4, 5, 2, 6, 5} };
+	cout << solution83(triangle);
+	return 0;
+}
