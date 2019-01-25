4be3da2b6e73a62bc5ffebed0bcd712026b247c9
wh2per <93negaro@gmail.com>
2019-01-26 04:18:44 +0900

    Lv3_예산

---
+++ Programmers/Lv3/Lv3_예산.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+int solution89(vector<int> budgets, int M) {
+	int answer = -1;
+	int sum = 0;
+	int temp = 0;
+	for (int i = 0; i < budgets.size(); i++) {
+		if (answer < budgets[i])
+			answer = budgets[i];
+		sum += budgets[i];
+	}
+	temp = sum;
+	while (temp > M) {
+		temp = sum;
+		answer--;
+		for (int i = 0; i < budgets.size(); i++) {
+			if (answer < budgets[i])
+				temp -= budgets[i] - answer;
+		}
+	}
+
+	return answer;
+}
