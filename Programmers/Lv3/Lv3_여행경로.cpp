f9d33c3ef93f56f2565b938acbfc29be0d94f628
wh2per <93negaro@gmail.com>
2019-01-26 03:50:18 +0900

    Lv3_여행경로

---
+++ Programmers/Lv3/Lv3_여행경로.cpp
@@ -0,0 +1,45 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<bool> check;
+vector<string> ans;
+string tmp = "a";
+
+void DFS(vector<vector<string>> tickets, string end, int count, string path) {
+	if (count >= tickets.size()) {
+		string p = path;
+		if (tmp > p)
+			tmp = p;
+		return;
+	}
+
+	for (int i = 0; i < tickets.size(); i++) {			// 사용할 티켓 찾기
+		if (tickets[i][0] == end && check[i] == false) {
+			check[i] = true;
+			ans.push_back(tickets[i][1]);
+			DFS(tickets, tickets[i][1], count + 1, path + tickets[i][1]);
+			ans.pop_back();
+			check[i] = false;
+		}
+	}
+}
+
+vector<string> solution88(vector<vector<string>> tickets) {
+	vector<string> answer;
+	check.resize(tickets.size());
+	ans.push_back("ICN");
+	DFS(tickets, "ICN", 0, "ICN");
+
+	for (int i = 0; i < tmp.size(); i += 3)
+		answer.push_back(tmp.substr(i, 3));
+
+	return answer;
+}
+
+int main() {
+	vector<vector<string>> tickets = { {"ICN", "JFK"},{"JFK", "ICN"},{"ICN", "JFK" }};
+	vector<string> answer = solution88(tickets);
+	return 0;
+}
