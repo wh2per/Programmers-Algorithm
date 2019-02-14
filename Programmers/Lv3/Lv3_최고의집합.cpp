7c1a1c180c84a0e927cdcb2b52e839d2ffe88171
wh2per <93negaro@gmail.com>
2019-02-15 04:25:44 +0900

    Lv3_최고의집합

---
+++ Programmers/Lv3/Lv3_최고의집합.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<int> solution108(int n, int s) {
+	vector<int> answer;
+	if (s / n == 0)
+		return { -1 };
+	else {
+		int q = s / n;
+		int r = s % n;
+		int j = n - 1;
+		for(int i=0; i<n; i++)
+			answer.push_back(q);
+		while (r != 0) {
+			answer[j--]++;
+			r--;
+		}
+	}
+	return answer;
+}
+
