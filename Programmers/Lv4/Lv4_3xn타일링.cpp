a7a751bcced98e7138744e42144cde4bc2cbaa0b
wh2per <93negaro@gmail.com>
2019-02-16 01:04:32 +0900

    Lv4_3xn타일링

---
+++ Programmers/Lv4/Lv4_3xn타일링.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution110(int n) {
+	vector<long long> d(n + 1, 0);
+	d[0] = 1;
+	for (int i = 2; i <= n; i += 2) {
+		d[i] = d[i - 2] * 3;
+		for (int j = i - 4; j >= 0; j -= 2) {
+			d[i] += d[j] * 2;
+		}
+		d[i] = d[i] % 1000000007;
+	}
+	return d[n];
+}
