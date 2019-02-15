5f4ba0abe9e076c2bbf045c07a4b2197feb73551
wh2per <93negaro@gmail.com>
2019-02-15 22:30:53 +0900

    Lv3_기지국설치

---
+++ Programmers/Lv3/Lv3_기지국설치.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int solution109(int n, vector<int> stations, int w)
+{
+	int answer = 0;
+	int index = 0;
+
+	for (int i = 1; i <= n; i++) {
+		if (index<stations.size() && i >= stations[index] - w) {
+			i = stations[index] + w;
+			index++;
+		}
+		else {
+			i += (2 * w);
+			if (i > n)
+				i = n;
+			answer++;
+		}
+	}
+
+	return answer;
+}
