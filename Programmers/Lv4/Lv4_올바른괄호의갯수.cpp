5f46a92f437bea3af3b7890e537d018a6991ec96
wh2per <93negaro@gmail.com>
2019-03-04 00:37:05 +0900

    Lv4_올바른괄호의갯수

---
+++ Programmers/Lv4/Lv4_올바른괄호의갯수.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+using namespace std;
+
+// Ä«Å»¶õ ¼ö -> Cn = C0*Cn-1 + C1*Cn-2 + C2*Cn-3 + ... + Cn-1C0
+int solution117(int n) {
+	int answer = 0;
+	vector<int> d;
+	d.push_back(1); //C0 = 1;
+	d.push_back(1); //C1 = 1;
+
+	for (int i = 2; i <= n; i++) {
+		int sum = 0;
+		for (int j = 0; j < i; j++) {
+			int k = i - j - 1;
+			sum += d[j] * d[k];
+		}
+		d.push_back(sum);
+	}
+	return d[n];
+}
+
