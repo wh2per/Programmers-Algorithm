377d1f9f76230036f2ff18680d5a298ede371b3f
wh2per <93negaro@gmail.com>
2019-03-05 14:16:29 +0900

    Lv4_숫자블록

---
+++ Programmers/Lv4/Lv4_숫자블록.cpp
@@ -0,0 +1,33 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+long long get_num(long long target) {		// 자신을 제외한 최대 약수를 반환
+	long long i;
+	for (i = 2; i*i < target; i++) {
+		if (target%i==0)
+			break;
+	}
+	if (i*i > target)
+		return 1;
+	return target / i;
+}
+
+// 각 자리는 최대 약수가 들어감
+vector<int> solution120(long long begin, long long end) {
+	vector<int> answer;
+	long long start_block = 1;
+	long long end_block = end >> 1;
+
+	if (begin == 1) {
+		answer.push_back(0);
+		begin++;
+	}
+
+	for (long long i = begin; i <= end; i++) {
+		answer.push_back(get_num(i));
+	}
+
+	return answer;
+}
