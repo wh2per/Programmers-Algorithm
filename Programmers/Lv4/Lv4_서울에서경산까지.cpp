ce91f3f91b2cc94da33d6235776840bbc466e3e8
wh2per <93negaro@gmail.com>
2019-03-16 00:20:38 +0900

    Lv4_서울에서경산까지

---
+++ Programmers/Lv4/Lv4_서울에서경산까지.cpp
@@ -0,0 +1,25 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+// BFS가 DFS보다 탐색 횟수가 더 많음
+// DP가 가장 빠름
+
+int solution124(int tK, vector<vector<int>> travel) {
+	vector<int> d(100003);
+	int tsize = travel.size();
+	
+	for (int i = 0; i < tsize; i++) {
+		for (int j = tK; j >= 0; j--) {
+			d[j] = INT_MIN;
+			if (j >= travel[i][0])
+				d[j] = max(d[j], d[j - travel[i][0]] + travel[i][1]);
+			if (j >= travel[i][2])
+				d[j] = max(d[j], d[j - travel[i][2]] + travel[i][3]);
+		}
+	}
+	return d[tK];
+}
+
