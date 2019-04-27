cd3a54076aebb4c0533971f33f8df4446e81eb76
wh2per <93negaro@gmail.com>
2019-04-27 17:18:35 +0900

    Lv5_비밀지도

---
+++ Programmers/Lv5/Lv5_비밀지도.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> solution126(int n, vector<int> arr1, vector<int> arr2) {
+	vector<string> answer(n);
+	for (int i = 0; i < n; i++) {
+		int value = arr1[i] | arr2[i];
+		string temp = "";
+		for (int i = 0; i < n; ++i)
+			temp += " ";
+		answer[i] = temp;
+		for (int j = n - 1; j >= 0; j--) {
+			if (value % 2 == 0)
+				answer[i][j] = ' ';
+			else
+				answer[i][j] = '#';
+			value /= 2;
+		}
+	}
+	return answer;
+}
